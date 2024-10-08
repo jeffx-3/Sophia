7aa4c9332bbcdea796a996a3c835a3530b996ada
jeffx-3 <geoffreygroovy@gmail.com>
2024-10-08 14:51:19 +0300

    fixed a typo in the url

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home),
-     path('search/', views.search, name='search'),
+    path('search/', views.search, name='search'),
     path('Discover/', views.product_list, name='product_list'),
     path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
